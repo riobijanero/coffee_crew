77af7c4bcf0a17986581f4736a34cbcc8793d677
Bijan Sellahi <bisevilua.net@Bijans-MacBook-Pro.local>
2020-07-20 14:59:10 +0200

    add Multiprovider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:coffee_crew/models/user.dart';
-import 'package:coffee_crew/screens/wrapper.dart';
+import 'package:coffee_crew/modules/authentication/widgets/authentication_controller.dart';
+import 'package:coffee_crew/modules/dashboard/widgets/dashboard_controller.dart';
+import 'package:coffee_crew/modules/wrapper.dart';
 import 'package:coffee_crew/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,11 +11,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (context) => AuthenticationController(context)),
+        StreamProvider<User>.value(value: AuthService().user),
+        Provider<DashBoardController>(create: (_) => DashBoardController()),
+      ],
       child: MaterialApp(
         home: Wrapper(),
       ),
