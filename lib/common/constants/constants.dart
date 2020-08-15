55ce1eb4c55f6236684d1b7b1955b07b088220b5
Bijan Sellahi <bisevilua.net@Bijans-MacBook-Pro.local>
2020-08-15 16:47:07 +0200

    refactor textformfield decoration

--- lib/common/constants/constants.dart
+++ lib/common/constants/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const textInputDecoration = InputDecoration(
+const textInputDecoration1 = InputDecoration(
   fillColor: Colors.white,
   filled: true,
   enabledBorder: OutlineInputBorder(
@@ -14,5 +14,67 @@ const textInputDecoration = InputDecoration(
       color: Colors.pink,
       width: 2.0,
     ),
+  ),
+);
+
+final textInputDecoration = InputDecoration(
+  fillColor: Color(0xFF6CA8F1),
+  filled: true,
+  border: textFormFieldWhiteBorder,
+  enabledBorder: textFormFieldWhiteBorder,
+  focusedBorder: textFormFieldWhiteBorder,
+  errorBorder: textFormFieldErrorBorder,
+  errorStyle: textFormFieldErrorMessageStyle,
+  hintStyle: textFormFieldHintTextStyle,
+  contentPadding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 10.0),
+);
+
+final OutlineInputBorder textFormFieldWhiteBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(15.0),
+  borderSide: BorderSide(
+    color: Colors.white70,
+    width: 2.0,
+  ),
+);
+
+final OutlineInputBorder textFormFieldErrorBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(15.0),
+  borderSide: BorderSide(
+    color: Colors.red,
+    width: 2.0,
+  ),
+);
+
+final TextStyle textFormFieldFontStyle = TextStyle(
+  color: Colors.white,
+);
+
+final textFormFieldHintTextStyle = TextStyle(
+  color: Colors.white54,
+  fontFamily: 'OpenSans',
+);
+
+final TextStyle textFormFieldErrorMessageStyle = TextStyle(
+  fontSize: 14.0,
+);
+
+final textFormFieldLabelStyle = TextStyle(
+  fontSize: 16,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final BoxDecoration backgroundDecoration = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      Color(0xFF73AEF5),
+      Color(0xFF61A4F1),
+      Color(0xFF478DE0),
+      Color(0xFF398AE5),
+    ],
+    stops: [0.1, 0.4, 0.7, 0.9],
   ),
 );
