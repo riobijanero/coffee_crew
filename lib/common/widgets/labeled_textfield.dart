02e75d6437cd70abf5ee250511496faffd2c1ea7
Bijan Sellahi <bisevilua.net@Bijans-MacBook-Pro.local>
2020-08-16 15:03:35 +0200

    extract textfield to own widget

---
+++ lib/common/widgets/labeled_textfield.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:coffee_crew/common/constants/constants.dart';
+
+class LabeledTextfield extends StatelessWidget {
+  const LabeledTextfield(
+      {Key key,
+      @required this.validator,
+      @required this.onChanged,
+      this.labeltext,
+      this.hintText,
+      this.icon,
+      this.texteditController,
+      this.obscureText = false,
+      this.trailingIcon,
+      this.fillColor})
+      : super(key: key);
+
+  final String Function(String) validator;
+  final void Function(String value) onChanged;
+  final String labeltext;
+  final String hintText;
+  final Icon icon;
+  final IconButton trailingIcon;
+  final TextEditingController texteditController;
+  final bool obscureText;
+  final Color fillColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          labeltext,
+          style: textFormFieldLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        TextFormField(
+          keyboardType: TextInputType.text,
+          obscureText: obscureText,
+          controller: texteditController,
+          cursorColor: textFormFieldFontStyle.color,
+          style: textFormFieldFontStyle,
+          decoration: textInputDecoration.copyWith(
+            hintText: hintText,
+            prefixIcon: icon,
+            suffixIcon: (trailingIcon != null) ? trailingIcon : null,
+            fillColor: fillColor != null ? fillColor : null,
+          ),
+          validator: (value) => validator(value),
+          onChanged: (value) => onChanged(value),
+        ),
+      ],
+    );
+  }
+}
